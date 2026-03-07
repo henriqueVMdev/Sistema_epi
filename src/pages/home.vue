b7174a45f2e0f1f1eb9ff3af7c17c951e8368b53
henriqueVMdev <henrique.de.paula.valim@gmail.com>
2026-03-06 22:09:04 -0300

    Inicio da página home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,53 @@
 <template>
   <div>
-    <h1>Home</h1>
-    <p>Landing page</p>
+    <h1>logo que tenho em casa</h1>
+    <p>peça uma demonstração</p>
+    <p>Login</p>
   </div>
+
+  <Section id="home">
+    <h2>PROTEJA.</h2>
+    <h2>RASTREIE.</h2>
+    <h2>CONTROLE.</h2>
+    <p>
+      Reduza os custos da sua empresa aumentando o controle e eficiência
+      operacional na gestão de EPIs com as diversas ultilidades do software
+      OmniSeg e sinta uma mudança que você sente no bolso.
+    </p>
+  </Section>
+
+  <section id="apresentção">
+    <h3>Software completo e intuitivo</h3>
+    <p>A evolução digital que sua gestão precisa</p>
+    <button>Começar agora</button>
+    <div>video ira aqui</div>
+  </section>
+
+  <section id="funcionalidades">
+    <card>
+      <p>Rastreamento de estoque</p>
+      <span>
+        Controle total do inventário em tempo real. Saiba exatamente quantos
+        EPIs estão em uso, em estoque e quando reabastecer.
+      </span>
+    </card>
+
+    <card>
+      <p>Alertas Automáticos</p>
+      <span>
+        Receba notificações antes do vencimento do CA (Certificado de Aprovação)
+        ou da vida útil do equipamento entregue ao colaborador.
+      </span>
+    </card>
+
+    <card>
+      <p>Assinatura Digital</p>
+      <span>
+        Elimine o papel. Registre a entrega de EPIs com assinatura biométrica ou
+        digital, garantindo validade jurídica e organização.
+      </span>
+    </card>
+  </section>
 </template>
 
 <script>
