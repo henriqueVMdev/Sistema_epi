a311beef594ab75b60e1139c9bf55aaece96043f
henriqueVMdev <henrique.de.paula.valim@gmail.com>
2026-03-06 22:55:54 -0300

    continuação home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -25,7 +25,7 @@
 
   <section id="funcionalidades">
     <card>
-      <p>Rastreamento de estoque</p>
+      <h4>Rastreamento de estoque</h4>
       <span>
         Controle total do inventário em tempo real. Saiba exatamente quantos
         EPIs estão em uso, em estoque e quando reabastecer.
@@ -33,7 +33,7 @@
     </card>
 
     <card>
-      <p>Alertas Automáticos</p>
+      <h4>Alertas Automáticos</h4>
       <span>
         Receba notificações antes do vencimento do CA (Certificado de Aprovação)
         ou da vida útil do equipamento entregue ao colaborador.
@@ -41,12 +41,37 @@
     </card>
 
     <card>
-      <p>Assinatura Digital</p>
+      <h4>Assinatura Digital</h4>
       <span>
         Elimine o papel. Registre a entrega de EPIs com assinatura biométrica ou
         digital, garantindo validade jurídica e organização.
       </span>
     </card>
+  </section>
+
+  <section id="entrega">
+    <h5>Entrega e retirada registrados pelo app</h5>
+    <p>
+      Nossa tecnologia exclusiva utiliza reconhecimento facial para validar a
+      identidade do colaborador no momento da retirada do equipamento, gerando
+      uma ficha de EPI digital à prova de fraudes.
+    </p>
+
+    <div>
+      <p>Confirmação instantânea</p>
+    </div>
+
+    <div>
+      <p>Localização da entrega</p>
+    </div>
+
+    <div>
+      <p>Atualização de estoque automática</p>
+    </div>
+    <p>saiba mais sobre a tecnologia</p>
+  </section>
+  <section id="saiba_mais">
+    <p>vantagens</p>
   </section>
 </template>
 
