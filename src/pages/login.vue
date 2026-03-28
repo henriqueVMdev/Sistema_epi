d3a60fc7688846749ef414a4eda83f98208466e0
henriqueVMdev <henrique.de.paula.valim@gmail.com>
2026-03-27 22:25:02 -0300

    Instalação do supabase e configuração do ambiente

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,8 +1,21 @@
 <template>
-  <div>
-    <h1>Login</h1>
+  <div class="container">
+    <H1>Login</H1>
     <p>Entre com suas credenciais</p>
+    <!--@submit.prevent impede de carregar a pagina -->
+    <form @submit.prevent="fazer login">
+      <div class = "campo">
+        <label> email</label>
+        <!--v-model= liga o campo variavel com o email-->
+        <input type="email" v-model="email" required /></div>
+        <label> senha </label>
+        <input type="password" v-model="senha" required />
+        <!--V-if Exibe mensagem de erro, se houver -->
+        <p v-if="erro" {{ erro }}></p>
+        <button type="submit">Entrar</button>
+    </form>
   </div>
+ 
 </template>
 
 <script>
