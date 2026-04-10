d2e38fcb128402a90e80c2d9407acea8e034420a
henriqueVMdev <henrique.de.paula.valim@gmail.com>
2026-04-10 20:26:56 -0300

    UPDATE Login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,22 +1,19 @@
 <template>
-  <div class="container">
-    <H1>Login</H1>
-    <p>Entre com suas credenciais</p>
-    <!--@submit.prevent impede de carregar a pagina -->
-    <form @submit.prevent = "fazerLogin">
-      <div class = "campo">
-        <label> email</label>
-        <!--v-model= liga o campo variavel com o email-->
-        <input type="email" v-model= "email" required /> </div>
-        <label> senha </label>
-        <input type="password" v-model="senha" required />
-        <!--V-if Exibe mensagem de erro, se houver -->
-        <p v-if="erro" {{ erro }}></p>
-        <button type="submit">Entrar</button>
-    </form>
-  </div>
+  <div class="caixa">
+    <form @submit.prevent="login">
+      <label>Email:</label>
+      <input v-model = "email">
 
+      <label>Senha:</label>
+      <input v-model = "senha">
+
+    <button class = "btn">
+      Entrar
+    </button>
+  </form>
+  </div>
 </template>
+    
 
 <script>
 export default {
